a842cb4916c91dfe3f7d790b1237bd2faae19c98
Sára Hölclová <sara.holclova@gmail.com>
2024-06-01 14:09:15 +0200

    stavy v Joke již nepotřebuji, smazáno

--- src/components/Joke/Joke.jsx
+++ src/components/Joke/Joke.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react';
 import './Joke.css';
 
 export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
-  const [likesUp, setLikesUp] = useState(0);
-  const [dislikesDown, setDislikesDown] = useState(0);
-
   return (
     <div className="container">
       <div className="joke">
@@ -17,19 +13,11 @@ export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
           <p className="joke__text">{text}</p>
         </div>
         <div className="joke__likes">
-          <button
-            onClick={() => setLikesUp(likesUp + 1)}
-            id="btn-up"
-            className="btn-like btn-like--up"
-          ></button>
+          <button id="btn-up" className="btn-like btn-like--up"></button>
           <span id="likes-up" className="likes-count likes-count--up">
             {likes}
           </span>
-          <button
-            onClick={() => setDislikesDown(dislikesDown + 1)}
-            id="btn-down"
-            className="btn-like btn-like--down"
-          ></button>
+          <button id="btn-down" className="btn-like btn-like--down"></button>
           <span id="likes-down" className="likes-count likes-count--down">
             {dislikes}
           </span>

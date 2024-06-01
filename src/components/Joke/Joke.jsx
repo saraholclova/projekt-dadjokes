86b5ce8b3818f3e425eac28d37127b04d0fc69d8
Sára Hölclová <sara.holclova@gmail.com>
2024-06-01 13:12:27 +0200

    přidána komponenta Joke a importovány styly

---
+++ src/components/Joke/Joke.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+export const Joke = ({}) => {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            onClick={() => setLikes(likes + 1)}
+            id="btn-up"
+            className="btn-like btn-like--up"
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likes}
+          </span>
+          <button
+            onClick={() => setDislikes(dislikes + 1)}
+            id="btn-down"
+            className="btn-like btn-like--down"
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {dislikes}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};

3556734843967392b4593899b0b32ee3c6d85a87
Sára Hölclová <sara.holclova@gmail.com>
2024-06-01 16:35:42 +0200

    přidány stavy pro možnost zvyšovat počet likes a dislikes na kliknutí

--- src/components/Joke/Joke.jsx
+++ src/components/Joke/Joke.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import './Joke.css';
 
 export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [goodlike, setGoodlike] = useState(likes);
+  const [badlike, setBadlike] = useState(dislikes);
+
   return (
     <div className="container">
       <div className="joke">
@@ -13,13 +17,21 @@ export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
           <p className="joke__text">{text}</p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => setGoodlike(goodlike + 1)}
+          ></button>
           <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
+            {goodlike}
           </span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => setBadlike(badlike + 1)}
+          ></button>
           <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
+            {badlike}
           </span>
         </div>
       </div>
